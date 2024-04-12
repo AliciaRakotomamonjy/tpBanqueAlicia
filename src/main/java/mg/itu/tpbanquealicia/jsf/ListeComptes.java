003af83794d12e922a359a2bf3a1f5b22a6be39d
AliciaRakotomamonjy <aliciarakotomamonjy@gmail.com>
2024-04-12 09:24:19 +0300

    Création de la page liste des comptes avec primefaces et de l'accueil

---
+++ src/main/java/mg/itu/tpbanquealicia/jsf/ListeComptes.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquealicia.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanquealicia.entity.CompteBancaire;
+import mg.itu.tpbanquealicia.service.GestionnaireCompte;
+
+/**
+ *
+ * @author alici
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+    
+    private List<CompteBancaire> allCompteBancaires;
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+
+    public List<CompteBancaire> getAllCompteBancaires() {
+        if(allCompteBancaires == null){
+            allCompteBancaires = gestionnaireCompte.getAllCompteBancaires();
+        }
+        return allCompteBancaires;
+    }
+    
+    
+    
+}
---
+++ src/main/java/mg/itu/tpbanquealicia/jsf/TransfertArgent.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquealicia.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author alici
+ */
+@Named(value = "transfertArgent")
+@RequestScoped
+public class TransfertArgent {
+
+    /**
+     * Creates a new instance of TransfertArgent
+     */
+    public TransfertArgent() {
+    }
+    
+}
